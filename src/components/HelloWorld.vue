e4d0d3d14039d06b7a5f035c7eba1eba9d3aaf51
erfgerfs <2221952500@qq.com>
2023-12-25 20:58:11 +0800

    feat(homeview.vue): add

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -107,18 +107,22 @@
         >
       </li>
     </ul>
+    <p>11 wqdr safe 1</p>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: "HelloWorld",
+  name: 'HelloWorld',
   props: {
-    msg: String,
+    msg: String
   },
-});
+  created() {
+    console.log('aaa')
+  }
+})
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
